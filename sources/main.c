24816582809e4610b839b0c01efa20a57718fab0
Ramzi BOUGUERRA <rabougue@e1r11p23.42.fr>
2016-03-11 16:22:08 +0100

    fdf

--- sources/main.c
+++ sources/main.c
@@ -1,18 +1,16 @@
 #include "../includes/fdf.h"
+#include <unistd.h>
 
-int	count_y(void)
+
+int	count_y(int *fd)
 {
-	int		fd;
+	int		i;
 	char	*line;
-	int		j;
-	int		y;
 
-	while (get_next_line(fd, &line) == 1)
-	{
-		y++;
-		printf("%d\n", y);
-	}
-	return (y);
+	i = 0;
+	while ((get_next_line(*fd, &line)) > 0)
+		i++;
+	return (i);
 }
 
 int	main(int argc, char **argv)
@@ -20,31 +18,27 @@ int	main(int argc, char **argv)
 	void	*mlx;
 	void	*win;
 	int		fd;
-	int x;
-	int y;
-	int y2 = count_y();
+	int		x;
+	int		y;
+
+	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	{
+		ft_putendl("Impossible d'ouvrir le fichier.");
+		return (-1);
+	}
+	y = (count_y(&fd) * 10);
 	int	width = 1280;
-	int height = 720;
+	int	height = 720;
 	int	color = 0xFFFFFF;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, width, height, "fdf");
-	y = 0;
 	x = 0;
-	if (argc == 2)
+	while (y)
 	{
-		if ((fd = open(argv[1], O_RDONLY)) == -1)
-		{
-			ft_putendl("Impossible d'ouvrir le fichier !");
-			return (-1);
-		}
-		while (y <= y2)
-		{
-			mlx_pixel_put(mlx, win, x, y, color);
-			y++;
-			printf("%d\n", y2);
-		}
-		mlx_clear_window(mlx, win);
-		mlx_loop(mlx);
+		mlx_pixel_put(mlx, win, x, y, color);
+		y--;
 	}
+	/*mlx_clear_window(mlx, win);*/
+	mlx_loop(mlx);
 }
