1bcc803c158896d032ea96f006435280985f2dd3
Ramzi BOUGUERRA <rabougue@e1r11p23.42.fr>
2016-03-15 20:17:44 +0100

    fdf

--- Makefile
+++ Makefile
@@ -6,17 +6,17 @@
 #    By: rabougue <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/12/10 19:16:51 by rabougue          #+#    #+#              #
-#    Updated: 2016/03/14 19:23:36 by rabougue         ###   ########.fr        #
+#    Updated: 2016/03/14 19:24:19 by rabougue         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = fdf
 FLAG = -Wall -Wextra -Werror
-FRAMEWORK = -lmlx -framework OpenGL -framework AppKit
+FRAMEWORK = -framework OpenGL -framework AppKit
 SRCS = ./main.c
 OBJS = $(SRCS:.c=.o)
-LFT = ./libft/libft.a 
-LMLX = ./includes/libmlx.a
+LFT = -L./libft/ -lft
+LMLX = -L./minilibx_macos/ -lmlx
 HEAD = -I ./includes/fdf.h
 
 all: $(NAME)
@@ -25,6 +25,9 @@ $(NAME):
 	@echo "\033[33mCompiling libft.a ...\033[0m"
 	@make -s -C ./libft/
 	@echo "\033[32mCompiling libft.a done !\033[0m"
+	@echo "\033[33mCompiling mlx.a ...\033[0m"
+	@make -s -C ./minilibx_macos/
+	@echo "\033[32mCompiling mlx.a done !\033[0m"
 	@echo "\033[33mCompiling in .o ...\033[0m"
 	@gcc $(HEAD) -c $(SRCS)
 	@echo "\033[32mCompiling in .o done !\033[0m"
@@ -44,5 +47,6 @@ fclean:
 	@rm -f $(NAME) $(OBJS)
 	@echo "\033[32mThe folder is clean\033[0m"
 	@make -s fclean -C ./libft/
+	@make -s clean -C ./minilibx_macos/
 
 re: fclean all
--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "./includes/fdf.h"
 
+#define blue 0x23c5e3
+#define white 0xFFFFFF
+#define red 0xFF1616
+
 static int	count_y(int *fd)
 {
 	int		i;
@@ -11,32 +15,58 @@ static int	count_y(int *fd)
 	return (i);
 }
 
+static int	count_x(int *fd)
+{
+	char *line;
+	char **var;
+	int i = 0;
+
+	while ((get_next_line(*fd, &line)) > 0)
+	{
+		var = ft_strsplit(line, ' ');
+		while (var[i])
+			i++;
+	}
+	return (i);
+}
+
 int	main(int argc, char **argv)
 {
-	void	*mlx;
-	void	*win;
+	/*void	*mlx;*/
+	/*void	*win;*/
 	int		fd;
-	int		x;
-	int		y;
+	/*int		x;*/
+	/*int		y_pos;*/
+	/*int cmp;*/
 
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
 	{
 		ft_putendl("Impossible d'ouvrir le fichier.");
 		return (-1);
 	}
-	y = (count_y(&fd) * 10);
-	int	width = 1280;
-	int	height = 720;
-	int	color = 0xFFFFFF;
-
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, width, height, "fdf");
-	x = 0;
-	while (y)
-	{
-		mlx_pixel_put(mlx, win, x, y, color);
-		y--;
-	}
-	/*mlx_clear_window(mlx, win);*/
-	mlx_loop(mlx);
+	count_x(&fd);
+	/*y_pos = 200;*/
+	/*int	width = 1280;*/
+	/*int	height = 720;*/
+	/*int	color = red;*/
+	/*cmp = (count_y(&fd) * 10);*/
+	/*mlx = mlx_init();*/
+	/*win = mlx_new_window(mlx, width, height, "fdf");*/
+	/*while (cmp > 0)*/
+	/*{*/
+	/*x = 0;*/
+	/*mlx_pixel_put(mlx, win, x, y_pos, color);*/
+	/*if (cmp % 10 == 0)*/
+	/*{*/
+	/*while (x < 600)*/
+	/*{*/
+	/*mlx_pixel_put(mlx, win, x, y_pos, color);*/
+	/*x++;*/
+	/*}*/
+	/*}*/
+	/*y_pos++;*/
+	/*cmp--;*/
+	/*}*/
+	/*[>mlx_clear_window(mlx, win);<]*/
+	/*mlx_loop(mlx);*/
 }
