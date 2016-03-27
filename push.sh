ee25a65e79ca0bebdebf64e04375291df8472758
Ramzi BOUGUERRA <rabougue@e1r11p23.42.fr>
2016-03-27 17:37:02 +0200

    FDF

--- push.sh
+++ push.sh
@@ -1,25 +1,25 @@
 clear
-echo "\033[48;5;21mmake fclean\033[0m"
+echo "\033[36mmake fclean\033[0m"
 make fclean
 echo "Appuyer sur Entrée pour continuer..."
 read a
 clear
-echo "\033[48;5;21mgit status\033[0m"
+echo "\033[36mgit status\033[0m"
 git status
 echo "Appuyer sur Entrée pour continuer..."
 read a
 clear
-echo "\033[48;5;21mgit add\033[0m"
+echo "\033[36mgit add\033[0m"
 git add *
-echo "\033[48;5;21mgit status\033[0m"
+echo "\033[36mgit status\033[0m"
 git status
 echo "Appuyer sur Entrée pour continuer..."
 read a
 clear
-echo "\033[48;5;21mgit commit\033[0m"
+echo "\033[36mgit commit\033[0m"
 git commit -m "FDF"
 echo "Appuyer sur Entrée pour continuer..."
 read a
 clear
-echo "\033[48;5;21mgit push\033[0m"
+echo "\033[36mgit push\033[0m"
 git push
