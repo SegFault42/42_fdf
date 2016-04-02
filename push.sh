6a328cb1e8e0ebf8cdd8b72f35cb27a04f948233
Ramzi BOUGUERRA <rabougue@e1r11p23.42.fr>
2016-04-02 13:16:07 +0200

    maj

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/zsh/sh
 
 clear
 echo "\033[35mmake fclean\033[0m"
@@ -16,10 +16,9 @@ git add *
 echo "\033[35mgit status\033[0m"
 git status
 echo "\033[41mAppuyer sur Entrée pour git commit...\033[0m"
-read a
 clear
 echo "\033[35mgit commit\033[0m"
-echo -n "commit name :"
+echo "commit name :"
 read commit
 git commit -m "$commit"
 echo "\033[41mAppuyer sur Entrée pour git push...\033[0m"
