a21667a594ac6b13e4c85d636697ae0d5d7a4252
Ramzi BOUGUERRA <rabougue@e3r3p16.42.fr>
2016-04-08 16:01:28 +0200

    menu

--- sources/menu.c
+++ sources/menu.c
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 14:57:22 by rabougue          #+#    #+#             */
-/*   Updated: 2016/04/08 15:05:02 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/04/08 15:44:02 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 
 void	menu(t_context *c)
 {
-	mlx_string_put(c->mlx_ptr, c->win_ptr, 0, 0, GREEN, "Menu");
+	mlx_string_put(c->mlx_ptr, c->win_ptr, 0, -5, GREEN, "Menu");
+
+
+	/*int	fd;*/
+	/*char **line;*/
+
+	/*fd = open("../test", O_RDONLY);*/
+	/*while (get_next_line(fd, line) > 0)*/
+	/*{*/
+		/*printf("%s", *line);*/
+		/*mlx_string_put(c->mlx_ptr, c->win_ptr, 0, 0, GREEN, *line);*/
+	/*}*/
 }
