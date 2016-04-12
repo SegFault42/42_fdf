25c0f2d835ddedd87d0dc5f375b9d1b42fe564d2
Ramzi BOUGUERRA <rabougue@e3r3p16.42.fr>
2016-04-12 23:46:14 +0200

    modification de la couleur du fil de fer

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/25 18:03:01 by rabougue          #+#    #+#             */
-/*   Updated: 2016/04/12 20:59:13 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/04/12 23:45:39 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	main(int argc, char **argv)
 	c.mlx_ptr = mlx_init();
 	c.img_ptr = mlx_new_image(c.mlx_ptr, WIDTH, HEIGHT);
 	im.data = mlx_get_data_addr(c.img_ptr, &im.bpp, &im.sizeline, &im.endian);
-	im.img_color = mlx_get_color_value(c.mlx_ptr, RED);
+	im.img_color = mlx_get_color_value(c.mlx_ptr, GREEN);
 	c.pti = &im;
 	c.win_ptr = mlx_new_window(c.mlx_ptr, WIDTH, HEIGHT, "FDF");
 
