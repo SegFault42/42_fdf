9ae9f590f046934ce408f3e1d56d20d39fc0c5b9
Ramzi BOUGUERRA <rabougue@e3r13p1.42.fr>
2016-04-13 20:59:01 +0200

    40 erreurs de norme

--- sources/key_hook.c
+++ sources/key_hook.c
@@ -6,35 +6,35 @@
 /*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/08 12:57:03 by rabougue          #+#    #+#             */
-/*   Updated: 2016/04/13 20:46:51 by rabougue         ###   ########.fr       */
+/*   Updated: 2016/04/13 20:57:50 by rabougue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-void		impr_img(void *param, t_pti *img, t_p *p, int gap, int level, int iso, int or_x, int or_y)
+void	impr_img(void *param, t_pti *img, t_p *p, int gap, int level, int iso, int or_x, int or_y)
 {
-		((t_context *)param)->pti->img_color = mlx_get_color_value(((t_context *)param)->mlx_ptr, GREEN);
-		print_p(((t_context *)param)->coord, ((t_context *)param)->pti, gap, level, iso, or_x, or_y);
-		mlx_put_image_to_window(((t_context *)param)->mlx_ptr, ((t_context *)param)->win_ptr, ((t_context *)param)->img_ptr, 0, 0);
+	((t_context *)param)->pti->img_color = mlx_get_color_value(((t_context *)param)->mlx_ptr, GREEN);
+	print_p(((t_context *)param)->coord, ((t_context *)param)->pti, gap, level, iso, or_x, or_y);
+	mlx_put_image_to_window(((t_context *)param)->mlx_ptr, ((t_context *)param)->win_ptr, ((t_context *)param)->img_ptr, 0, 0);
 }
 
 void	clear_image(void *param, t_pti *img, t_p *p)
 {
 		int i = 0;
-		int j = 0;
-		char *data;
+	int j = 0;
+	char *data;
 
-		data = mlx_get_data_addr(((t_context *)param)->img_ptr, &img->bpp, &img->sizeline, &img->endian);
-		while (i < (HEIGHT * img->sizeline))
-		{
-			data[i + 2] = 59;
-			data[i + 1] = 59;
-			data[i + 0] = 59;
-			data[i + 3] = 0;
-			i+=4;
-		}
-		mlx_put_image_to_window(((t_context *)param)->mlx_ptr, ((t_context *)param)->win_ptr, ((t_context *)param)->img_ptr, 0, 0);
+	data = mlx_get_data_addr(((t_context *)param)->img_ptr, &img->bpp, &img->sizeline, &img->endian);
+	while (i < (HEIGHT * img->sizeline))
+	{
+		data[i + 2] = 59;
+		data[i + 1] = 59;
+		data[i + 0] = 59;
+		data[i + 3] = 0;
+		i+=4;
+	}
+	mlx_put_image_to_window(((t_context *)param)->mlx_ptr, ((t_context *)param)->win_ptr, ((t_context *)param)->img_ptr, 0, 0);
 }
 
 int		key_hook(int keycode, void *param, t_pti *img, t_p *p)
@@ -45,6 +45,9 @@ int		key_hook(int keycode, void *param, t_pti *img, t_p *p)
 	int static	or_x = WIDTH / 2;
 	int static	or_y = HEIGHT / 3;
 
+	gap = 20;
+	level = 20;
+	iso = 2;
 	if (keycode == KEY_ESC)
 	{
 		mlx_destroy_image(((t_context *)param)->mlx_ptr, ((t_context *)param)->img_ptr);
